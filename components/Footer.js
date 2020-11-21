71d3b69f173adeadbbdc38a24accd15fe0440a01
Kailas Walldoddi <Kailas.Walldoddi@ellucian.com>
2020-11-21 19:34:26 +0530

    added moicons instead of titles in mobile view

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ const currentYear = d.getFullYear();
 function Footer() {
     return (
         <footer className={styles.footer}>
-           &copy; Copyright {currentYear}. With Love by {' '} <a href="https://www.linkedin.com/in/kailashw/" target="_blank"><span>Kailas Walldoddi</span> </a>
+           &copy; Copyright {currentYear}. <a href="https://www.linkedin.com/in/kailashw/" target="_blank"><span>Kailas Walldoddi</span> </a>
         </footer>
     )
 }
--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import Image from 'next/image'
 import Link from 'next/Link'
 import styles from "./header.module.css";
 class Header extends Component {
     render() {
         return (
             <div className={styles.header}>
-                <div className="mobile-view">
-                    <button onClick={myFunction}> <img src='/menu.png' alt="menu-icon" width="32" height="32" /> </button>
-                </div>
-                <div id="myDIV">
+                <div className={styles.mobileView}>
                     <ul>
-                        <li><Link href="/"><a href="#"> Home</a></Link></li>
-                        <li><Link href="/about"><a href="#"> About</a></Link></li>
-                        <li><Link href="/blogs"><a href="#"> Blogs</a></Link></li>
-                        <li><Link href="/projects"><a href="#"> Projects</a></Link></li>
-                        <li><a href="mailto:kailashwall@gmail.com" target="_blank"> Contact</a></li>
+                        <li><Link href="/" title="Home"><img src='/home.png' alt="home-icon" width="32" height="32" /></Link></li>
+                        <li><Link href="/about" title="About"><img src='/about.png' alt="about-icon" width="32" height="32" /></Link> </li>
+                        <li><Link href="/blogs" title="Blogs"><img src='/blogs.png' alt="blogs-icon" width="32" height="32" /></Link> </li>
+                        <li><Link href="/projects" title="Projects"><img src='/projects.png' alt="projects-icon" width="32" height="32" /></Link> </li>
+                        <li><a href="mailto:kailashwall@gmail.com" title="Contact" target="_blank"><img src='/contact.png' alt="contact-icon" width="32" height="32" /></a> </li>
                     </ul>
                 </div>
-                <div className="web-view">
+                <div className={styles.webView}>
                     <ul>
                         <li><Link href="/"><a href="#"> Home</a></Link></li>
                         <li><Link href="/about"><a href="#"> About</a></Link></li>
@@ -29,15 +25,6 @@ class Header extends Component {
                 </div>
             </div>
         )
-    }
-}
-
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (!x.style.display || x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
     }
 }
 
